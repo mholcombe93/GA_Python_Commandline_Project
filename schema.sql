a43ec52dea61befa1b4b497f3a348bba4a609627
Matthew Holcombe <matthewholcombe@Matthews-MacBook-Pro-8.local>
2022-11-17 15:30:56 -0500

    basic 2 tables works

---
+++ schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS contact_name; --deletes if already exists
+DROP TABLE IF EXISTS contact_location;
+
+CREATE TABLE contact_name (
+  id SERIAL PRIMARY KEY,
+  person_name VARCHAR(255),
+  nationality VARCHAR(255),
+  birth_year INTEGER
+);
+
+CREATE TABLE contact_location (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255),
+  phone_number INTEGER,
+  state_abreviation VARCHAR(2),
+  contact_name_id INTEGER
+);
